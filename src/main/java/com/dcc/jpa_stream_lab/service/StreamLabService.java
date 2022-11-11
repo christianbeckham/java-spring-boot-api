9e0c046a786d6d6e3ba6b07b81f99b6ea8b1fe42
Christian Beckham <urenacb22@gmail.com>
2022-11-11 17:51:26 -0600

    feat: completed read problems and first post problem

--- src/main/java/com/dcc/jpa_stream_lab/service/StreamLabService.java
+++ src/main/java/com/dcc/jpa_stream_lab/service/StreamLabService.java
@@ -1,11 +1,16 @@
 package com.dcc.jpa_stream_lab.service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -46,7 +51,7 @@ public class StreamLabService {
         // Return the COUNT of all the users from the User table.
         // You MUST use a .stream(), don't listen to the squiggle here!
         // Remember yellow squiggles are warnings and can be ignored.
-    	return 0;
+    	return users.findAll().stream().count();
     }
 
     public List<Product> RDemoTwo()
@@ -59,32 +64,34 @@ public class StreamLabService {
     {
         // Write a query that gets each product whose price is less than or equal to $100.
         // Return the list
-        return null;
+        return products.findAll().stream().filter(p -> p.getPrice() <= 100).toList();
     }
 
     public List<Product> RProblemThree()
     {
         // Write a query that gets each product that CONTAINS an "s" in the products name.
         // Return the list
-    	return null;
+    	return products.findAll().stream().filter(p -> p.getName().contains("s")).toList();
     }
 
-    public List<User> RProblemFour()
-    {
+    public List<User> RProblemFour() throws ParseException {
         // Write a query that gets all the users who registered BEFORE 2016
         // Return the list
         // Research 'java create specific date' and 'java compare dates'
         // You may need to use the helper classes imported above!
-    	
-        return null;
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+        Date year = yearFormat.parse("2016");
+        return users.findAll().stream().filter(u -> u.getRegistrationDate().before(year)).toList();
     }
 
-    public List<User> RProblemFive()
-    {
+    public List<User> RProblemFive() throws ParseException {
         // Write a query that gets all of the users who registered AFTER 2016 and BEFORE 2018
         // Return the list
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+        Date yearBefore = yearFormat.parse("2018");
+        Date yearAfter = yearFormat.parse("2016");
 
-        return null;
+        return users.findAll().stream().filter(u -> u.getRegistrationDate().before(yearBefore) && u.getRegistrationDate().after(yearAfter)).toList();
     }
 
     // <><><><><><><><> R Actions (Read) with Foreign Keys <><><><><><><><><>
@@ -102,17 +109,18 @@ public class StreamLabService {
     {
         // Write a query that retrieves all of the products in the shopping cart of the user who has the email "afton@gmail.com".
         // Return the list
-
-    	return null;
+        User user = users.findAll().stream().filter(u -> u.getEmail().equals("afton@gmail.com")).findFirst().orElse(null);
+        List<Product> items = shoppingcartitems.findAll().stream().filter(i -> i.getUser().getId() == user.getId()).map(p -> p.getProduct()).toList();
+    	return items;
     }
 
     public long RProblemSeven()
     {
         // Write a query that retrieves all of the products in the shopping cart of the user who has the email "oda@gmail.com" and returns the sum of all of the products prices.
     	// Remember to break the problem down and take it one step at a time!
-
-
-    	return 0;
+        List<ShoppingcartItem> items = shoppingcartitems.findAll().stream().filter(i -> i.getUser().getEmail().equals("oda@gmail.com")).toList();
+        Integer total = items.stream().reduce(0, (acc, p) -> acc + p.getProduct().getPrice(), Integer::sum);
+    	return total;
 
     }
 
@@ -120,8 +128,9 @@ public class StreamLabService {
     {
         // Write a query that retrieves all of the products in the shopping cart of users who have the role of "Employee".
     	// Return the list
-
-    	return null;
+        Role employeeRole = roles.findAll().stream().filter(r -> r.getName().equals("Employee")).findFirst().orElse(null);
+        List<Product> items = shoppingcartitems.findAll().stream().filter(i -> i.getUser().getRoles().contains(employeeRole)).map(p -> p.getProduct()).toList();
+    	return items;
     }
 
     // <><><><><><><><> CUD (Create, Update, Delete) Actions <><><><><><><><><>
@@ -142,10 +151,12 @@ public class StreamLabService {
     {
         // Create a new Product object and add that product to the Products table.
         // Return the product
-    	
-
-    	return null;
-
+        Product newProduct = new Product();
+        newProduct.setName("AirPods");
+        newProduct.setDescription("Wireless ear buds made by Apple.");
+        newProduct.setPrice(200);
+        products.save(newProduct);
+    	return newProduct;
     }
 
     public List<Role> CDemoTwo()
